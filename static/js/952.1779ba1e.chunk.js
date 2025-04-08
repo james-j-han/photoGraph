66d784bcf65efcb272f73d59e37cb159a0e531e0
James Han <jayimshan@gmail.com>
2025-04-08 01:34:10 -0400

    Updates

---
+++ static/js/952.1779ba1e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcapstone_2=self.webpackChunkcapstone_2||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.1779ba1e.chunk.js.map
